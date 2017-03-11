d92c5c9689f88da0c0573eb997a792e514d28a72
bsp <bsp0911932@163.com>
2017-03-11 19:22:45 +0800

    update Help

--- src/main/resources/helpContent.groovy
+++ src/main/resources/helpContent.groovy
@@ -4,7 +4,7 @@
 //              to leave a comment, open an issue or check the README there
 //          在gradle中配置showHelp = false 来关闭这个帮助内容的输出
 //          欢迎访问 https://github.com/BryanSharp/hibeaver 留下建议
+//           Hope you enjoy!
 //
 // author BryanSharp bsp0911932@163.com
 ///////////////////// - HiBeaver Help Content END - /////////////////////////////
-
